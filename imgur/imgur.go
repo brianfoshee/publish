47f34fe120a3783e5f38c6a0ea2ea782d1677f0f
Brian Foshee <brianfoshee@gmail.com>
2018-12-29 20:36:44 -0500

    bump to 50 galleries per page for now

--- imgur/imgur.go
+++ imgur/imgur.go
@@ -145,7 +145,8 @@ func Build(path string, drafts bool) {
 
 	// write out main feed, and pages if more than 10 galleries
 	// main feed is index 0-9. next page should be 10-19
-	const galleriesPerPage = 20
+	// TODO paginate. for now this does 50 pages at a time.
+	const galleriesPerPage = 50
 	total := len(galleries)
 	pages := math.Ceil(float64(total) / galleriesPerPage)
 	for i := 1; i <= int(pages); i += 1 {
