51e1327cec8b6eb3f4b3cb9ed63e519b8434409d
Brian Foshee <brianfoshee@gmail.com>
2019-09-13 10:42:34 -0400

    update go modules

---
+++ publish.go
@@ -0,0 +1,6 @@
+// Package publish is here so I can have a root-level go modules file instead
+// of needing to have one for every sub package. Hacky, but I do have some
+// actual functionality planned for this later.
+package publish
+
+const wat = "hi"
